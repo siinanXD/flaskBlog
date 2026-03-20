1245aa74e9748a0ee0b73c601d74b23582d64a8b
Sinan <sinankahraman@hotmail.de>
2026-03-20 14:59:16 +0100

    creat index,add html template and add my routes in app.py

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template("index.html", posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        title = request.form.get("title")
+        content = request.form.get("content")
+
+        # neue ID erzeugen
+        new_id = len(blog_posts) + 1
+
+        new_post = {
+            "id": new_id,
+            "title": title,
+            "content": content
+        }
+
+        blog_posts.append(new_post)
+
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
